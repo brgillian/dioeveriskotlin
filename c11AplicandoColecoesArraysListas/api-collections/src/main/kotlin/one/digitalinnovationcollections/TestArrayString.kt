7cc18e25d1011b951c74bedbe46cec6abe1fe0bc
brgillian <brgillian@gmail.com>
2021-03-03 15:31:34 -0300

    String arrayOf

---
+++ c11AplicandoColecoesArraysListas/api-collections/src/main/kotlin/one/digitalinnovationcollections/TestArrayString.kt
@@ -0,0 +1,26 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    fun separador() {
+        println("----------------------------------------------")
+    }
+
+    val nomes = Array(3) { "" } //Poderia ser: Array <String>(3){""}
+
+    nomes[0] = "Maria"
+    nomes[1] = "Zaza"
+    nomes[2] = "José"
+
+    for (nome in nomes) {
+        println(nome)
+    }
+    separador()
+
+    nomes.sort()
+    nomes.forEach { println(it) }
+    separador()
+
+    val nomes2 = arrayOf("Maria", "Zazá", "Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
