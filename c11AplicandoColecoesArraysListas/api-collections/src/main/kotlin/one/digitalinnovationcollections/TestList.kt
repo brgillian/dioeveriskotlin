31cf8dc6eab1c8f2f4fbb31724a9c94b582cfa84
brgillian <brgillian@gmail.com>
2021-03-03 18:24:11 -0300

    Formatação de string

---
+++ c11AplicandoColecoesArraysListas/api-collections/src/main/kotlin/one/digitalinnovationcollections/TestList.kt
@@ -0,0 +1,29 @@
+package one.digitalinnovationcollections
+fun main(){
+
+    fun separador() {
+        println("----------------------------------------------")
+    }
+
+    val joao = Funcionario("Joao",  1000.0)
+    val pedro = Funcionario("Pedro", 2000.0)
+    val maria = Funcionario("Maria", 4000.0)
+
+    val funcionarios = listOf(joao, pedro, maria)
+    funcionarios.forEach { println(it) }
+    separador()
+
+    println(funcionarios.find {it.nome == "Maria" })
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double
+) {
+    override fun toString(): String =
+        """
+            Nome: $nome
+            Salário: $salario
+            
+        """.trimIndent()
+}
