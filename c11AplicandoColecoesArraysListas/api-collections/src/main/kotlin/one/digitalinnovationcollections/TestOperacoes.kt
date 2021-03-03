49ec730de8a94bd40fe408df899b5896722997b6
brgillian <brgillian@gmail.com>
2021-03-03 17:22:24 -0300

    find count any - operacoes com arrays

--- c11AplicandoColecoesArraysListas/api-collections/src/main/kotlin/one/digitalinnovationcollections/TestOperacoes.kt
+++ c11AplicandoColecoesArraysListas/api-collections/src/main/kotlin/one/digitalinnovationcollections/TestOperacoes.kt
@@ -1,11 +1,11 @@
 package one.digitalinnovationcollections
-
+// Vídeo 07: Operações com Array max, min, average e filter
 fun main(){
     fun separador() {
         println("----------------------------------------------")
     }
 
-    val salarios = doubleArrayOf(1000.0, 2501.0, 400.0)
+    val salarios = doubleArrayOf(1000.0, 2501.0, 400.0, 2250.0)
 
     for (salario in salarios){
         println(salario)
@@ -19,5 +19,22 @@ fun main(){
 
     val salariosMaioresQue2500 = salarios.filter{ it > 2500.0 }
     salariosMaioresQue2500.forEach { println(it) }
+    separador()
+
+    // Vídeo 8: Operações com Arrays count, find e any
+
+    println(salarios.count { it in 2000.0 ..5000.0 })
+    separador()
+
+    println(salarios.find { it == 2250.0 })
+    separador()
+
+    println(salarios.find{ it == 500.0 })
+    separador()
+
+    println(salarios.any { it == 1000.0 })
+    separador()
+
+    println(salarios.any { it == 500.0 })
     separador()
 }
