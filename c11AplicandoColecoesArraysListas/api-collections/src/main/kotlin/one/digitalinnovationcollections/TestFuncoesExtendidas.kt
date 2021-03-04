3d010798b779f4764629d6bf4b3dbf455b7e283e
brgillian <brgillian@gmail.com>
2021-03-04 16:29:34 -0300

    funcoes extendidas

---
+++ c11AplicandoColecoesArraysListas/api-collections/src/main/kotlin/one/digitalinnovationcollections/TestFuncoesExtendidas.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovationcollections
+
+fun main(){
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "1500". toBigDecimal(),
+        "4000". toBigDecimal()
+    )
+    println(salarios.somatoria())
+    Separador()
+
+    println(salarios.media())
+}
