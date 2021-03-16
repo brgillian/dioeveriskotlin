9eb0e656aadcebfa87efade29ea3ac9373f4e8ff
brgillian <brgillian@gmail.com>
2021-03-15 22:33:28 -0300

    Criacao da recyclerview

--- Lista/app/src/main/java/com/gillian/lista/MainActivity.kt
+++ Lista/app/src/main/java/com/gillian/lista/MainActivity.kt
@@ -2,10 +2,19 @@ package com.gillian.lista
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+    private fun bindViews(){
+
     }
 }
