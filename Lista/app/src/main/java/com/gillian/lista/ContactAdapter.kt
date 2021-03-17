a95008ee8182384247d060cb69c76ea08679c8d5
brgillian <brgillian@gmail.com>
2021-03-17 11:27:13 -0300

    Adicionando menu

--- Lista/app/src/main/java/com/gillian/lista/ContactAdapter.kt
+++ Lista/app/src/main/java/com/gillian/lista/ContactAdapter.kt
@@ -6,33 +6,39 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.gillian.lista.R.layout.contact_item
 
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>(){
 
     private val list: MutableList<Contact> = mutableListOf()
 
+    // Criação da view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ContactAdapterViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(contact_item,parent,false)
         return ContactAdapterViewHolder(view)
     }
 
+    // Populando a view
     override fun onBindViewHolder(holder:ContactAdapterViewHolder, position: Int) {
         holder.bind(list[position])
     }
 
+    // Pegando o tamanho da lista
+    override fun getItemCount(): Int {
+       return list.size
+    }
+
+    // Passa a lista externa para dentro do Adapter
     fun updateList(list:List<Contact>){
         this.list.clear()
         this.list.addAll(list)
         notifyDataSetChanged()
     }
 
-    override fun getItemCount(): Int {
-       return list.size
-    }
     class ContactAdapterViewHolder (itemView: View): RecyclerView.ViewHolder(itemView){
         private val tvName: TextView = itemView.findViewById(R.id.tv_name)
         private val tvTelefone: TextView = itemView.findViewById(R.id.tv_telefone)
-        private val ivFoto:ImageView = itemView.findViewById(R.id.iv_foto)
+        private val ivFoto: ImageView = itemView.findViewById(R.id.iv_foto)
 
         fun bind(contact:Contact){
             tvName.text = contact.nome
--- Lista/app/src/main/java/com/gillian/lista/MainActivity.kt
+++ Lista/app/src/main/java/com/gillian/lista/MainActivity.kt
@@ -2,19 +2,88 @@ package com.gillian.lista
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private val rvList: RecyclerView by lazy {
         findViewById<RecyclerView>(R.id.rv_list)
     }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initDrawer()
         bindViews()
+        updateList()
     }
-    private fun bindViews(){
 
+    // inicialização do drawerlayout
+    private fun initDrawer(){
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        var toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    // instância o recyclerview
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adapter.updateList(
+            arrayListOf(
+                    Contact(
+                            "Gillian Gomes",
+                            "(00) 0000-0000",
+                            "img.png"
+                    ),
+                    Contact(
+                            "Gloria Maria",
+                            "(99) 9999-9999",
+                            "img2.png"
+                    )
+            )
+        )
+    }
+
+    private fun showToast(menssage:String){
+        Toast.makeText(this, menssage, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item de menu 1")
+                true
+            }
+            R.id.item_menu_2 ->{
+                showToast("Exibindo item de menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
