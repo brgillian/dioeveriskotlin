6b270bdc45cd0e8ca2541044e0998f73c1803788
brgillian <brgillian@gmail.com>
2021-03-19 20:51:48 -0300

    Anotacoes sobre as funcoes de cada metodo do content provider

--- contentprovider/app/src/main/java/com/gillian/applicationcontentprovider/database/NotesProvider.kt
+++ contentprovider/app/src/main/java/com/gillian/applicationcontentprovider/database/NotesProvider.kt
@@ -7,10 +7,12 @@ import android.net.Uri
 
 class NotesProvider : ContentProvider() {
 
+    // Deleta os dados do provider
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
         TODO("Implement this to handle requests to delete one or more rows")
     }
 
+    // Retorna dados de imagem, arquivos...
     override fun getType(uri: Uri): String? {
         TODO(
             "Implement this to handle requests for the MIME type of the data" +
@@ -18,14 +20,18 @@ class NotesProvider : ContentProvider() {
         )
     }
 
+    // Insere dados na aplicação através do content privider
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
         TODO("Implement this to handle requests to insert a new row.")
     }
 
+    // Inicia tudo. Faz a instância dos bancos de dados, URLs...
     override fun onCreate(): Boolean {
         TODO("Implement this to initialize your content provider on startup.")
     }
 
+    // Seleciona arquivos dentro do sistema operacional, seleciona banco de dados...
+    // Sempre retorna um cursor
     override fun query(
         uri: Uri, projection: Array<String>?, selection: String?,
         selectionArgs: Array<String>?, sortOrder: String?
@@ -33,6 +39,7 @@ class NotesProvider : ContentProvider() {
         TODO("Implement this to handle query requests from clients.")
     }
 
+    // Atualiza o ID do content provider
     override fun update(
         uri: Uri, values: ContentValues?, selection: String?,
         selectionArgs: Array<String>?
