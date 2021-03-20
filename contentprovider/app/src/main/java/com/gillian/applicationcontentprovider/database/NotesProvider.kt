e2085afc5e7b3ca8bb351e2c2891e5a26ae02e1f
brgillian <brgillian@gmail.com>
2021-03-19 23:16:40 -0300

    Criacao do metodo update

--- contentprovider/app/src/main/java/com/gillian/applicationcontentprovider/database/NotesProvider.kt
+++ contentprovider/app/src/main/java/com/gillian/applicationcontentprovider/database/NotesProvider.kt
@@ -88,7 +88,15 @@ class NotesProvider : ContentProvider() {
         uri: Uri, values: ContentValues?, selection: String?,
         selectionArgs: Array<String>?
     ): Int {
-        TODO("Implement this to handle requests to update one or more rows.")
+        if(mUriMatcher.match(uri) == NOTES_BY_ID){
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffect = db.update(TABLE_NOTES, "$_ID=?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return linesAffect
+        }else{
+            throw UnsupportedOperationException("Uri não implementada")
+        }
     }
 
     companion object{
