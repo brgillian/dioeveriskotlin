bfff999399ba811bf457a440559446a614d78237
brgillian <brgillian@gmail.com>
2021-03-20 14:37:43 -0300

    NoteClickedListener criado

--- contentprovider/app/src/main/java/com/gillian/applicationcontentprovider/NoteClickedListener.kt
+++ contentprovider/app/src/main/java/com/gillian/applicationcontentprovider/NoteClickedListener.kt
@@ -3,6 +3,6 @@ package com.gillian.applicationcontentprovider
 import android.database.Cursor
 
 interface NoteClickedListener {
-    fun notClickedItem(cursor: Cursor)
+    fun noteClickedItem(cursor: Cursor)
     fun noteRemoveItem(cursor: Cursor?)
 }
