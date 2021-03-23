bc2e96cd100c9feb2f1718e6ac25541dd31eb161
brgillian <brgillian@gmail.com>
2021-03-23 10:34:11 -0300

    NotesDetailFragment adicionado porem com problemas

--- contentprovider/app/src/main/java/com/gillian/applicationcontentprovider/NotesDetailFragment.kt
+++ contentprovider/app/src/main/java/com/gillian/applicationcontentprovider/NotesDetailFragment.kt
@@ -1,20 +1,28 @@
 package com.gillian.applicationcontentprovider
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
-import android.content.ClipDescription
+import android.content.ContentValues
 import android.content.DialogInterface
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
+import com.gillian.applicationcontentprovider.database.NotesDatabaseHelper.Companion.DESCRIPTION_NOTES
+import com.gillian.applicationcontentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
+import com.gillian.applicationcontentprovider.database.NotesProvider.Companion.URI_NOTES
+
 
 class NotesDetailFragment: DialogFragment(), DialogInterface.OnCancelListener {
     private lateinit var noteEditTitle: EditText
     private lateinit var noteEditDescription: EditText
     private var id: Long = 0
 
-    companion object{
+    companion object {
         private const val EXTRA_ID = "id"
-        fun newInstance(id: Long): NotesDetailFragment{
+        fun newInstance(id: Long): NotesDetailFragment {
             val bundle = Bundle()
             bundle.putLong(EXTRA_ID, id)
 
@@ -24,15 +32,46 @@ class NotesDetailFragment: DialogFragment(), DialogInterface.OnCancelListener {
         }
     }
 
-    override fun onCreateDialog(savedInstanceState:Bundle?): Dialog {
-        val view = activity?.layoutInflater?.inflate(R.layout.note_detail, null)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view: View? = activity?.layoutInflater?.inflate(R.layout.note_detail, null)
 
         noteEditTitle = view?.findViewById(R.id.note_edt_title) as EditText
         noteEditDescription = view.findViewById(R.id.note_edt_description) as EditText
-        return super.onCreateDialog(savedInstanceState)
+
+
+        var newNote = true
+        if (arguments != null && arguments?.getLong(EXTRA_ID) != 0L) {
+            id = arguments?.getLong(EXTRA_ID) as Long
+            val uri = Uri.withAppendedPath(URI_NOTES, id.toString())
+            val cursor =
+                    activity?.contentResolver?.query(uri, null, null, null, null)
+
+            if (cursor?.moveToNext() as Boolean) {
+                newNote = false
+                noteEditTitle.setText(cursor.getString(cursor.getColumnIndex(TITLE_NOTES)))
+                noteEditDescription.setText(cursor.getString(cursor.getColumnIndex(DESCRIPTION_NOTES)))
+            }
+            cursor.close()
+        }
+        return AlertDialog.Builder(activity as Activity)
+                .setTitle(if(newNote) "Nova mensagem" else "Editar mensagem")
+                .setView(view)cd
+                .setPositiveButton("Salvar", this)
+                .setNegativeButton("Cancelar", this)
+                .create()
     }
 
-    /*override fun onClick(dialog: DialogInterface?, which: Int){
-        TODO("Não implementado")
-    }*/
+    override fun onClick(dialog: DialogInterface?,which: Int){
+        val values = ContentValues()
+        values.put(TITLE_NOTES, noteEditTitle.text.toString())
+        values.put(DESCRIPTION_NOTES, noteEditDescription.text.toString())
+
+        if(id != 0L){
+            val uri = Uri.withAppendedPath(URI_NOTES, id.toString())
+            context?.contentResolver?.update(uri, values, null, null)
+        }else{
+            context?.contentResolver?.insert(URI_NOTES, values)
+        }
+    }
+
 }
